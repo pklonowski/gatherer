7750c3dca1b06712d89b19eb53e0d43774c57c7a
pklonowski <paul.klonowskip@gmail.com>
2018-05-22 16:06:06 +0000

    p25

---
+++ spec/models/project_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe Project do
+  it "considers a project with no tasks to be done" do
+    project = Project.new
+    expect(project.done?).to be_truthy
+  end
+  it "knows that a project with an incomplete task is not done" do
+    project = Project.new
+    task = Task.new
+    project.tasks << task
+    expect(project.done?).to be_falsy
+  end
+end
